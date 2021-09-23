1b4c1f59654dcc15376e276ef647a79d3bada9f3
mayrapares <69501819+mayrapares@users.noreply.github.com>
2021-09-23 11:14:36 -0400

    setup

---
+++ js/portfoliosite.js
@@ -0,0 +1,23 @@
+$(document).ready(function() {
+ 
+    // Fakes the loading setting a timeout
+      setTimeout(function() {
+          $('body').addClass('loaded');
+      }, 3500);
+   
+  });
+
+  var coll = document.getElementsByClassName("collapsible");
+var i;
+
+for (i = 0; i < coll.length; i++) {
+  coll[i].addEventListener("click", function() {
+    this.classList.toggle("active");
+    var content = this.nextElementSibling;
+    if (content.style.display === "block") {
+      content.style.display = "none";
+    } else {
+      content.style.display = "block";
+    }
+  });
+}
